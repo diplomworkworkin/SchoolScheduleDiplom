4c1f799c51094da9d3d822ae9b0c06b553841f42
diplomworkworkin <poppippup13@gmail.com>
2026-02-12 09:55:32 +0300

    Disable edge resizing for shell windows and clean view code

--- SchoolScheduleApp/Views/TeacherWindow.xaml.cs
+++ SchoolScheduleApp/Views/TeacherWindow.xaml.cs
@@ -74,7 +74,6 @@ namespace SchoolScheduleApp.Views
         private void BtnMinimize_Click(object sender, RoutedEventArgs e)
             => WindowState = WindowState.Minimized;
 
-
         private void BtnMaximize_Click(object sender, RoutedEventArgs e)
             => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
 
